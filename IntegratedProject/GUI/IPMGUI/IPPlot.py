86c5bc69038cb0c8211593cd52e09105868813c7
csharp10-glitch <csharp10@uco.edu>
2022-06-05 17:45:40 -0500

    Started work on GUI. Using Tkinter. Have menubar and pyplot figure.

--- IntegratedProject/GUI/IPMGUI/IPPlot.py
+++ IntegratedProject/GUI/IPMGUI/IPPlot.py
@@ -5,25 +5,22 @@ matplotlib.use('TkAgg')
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import matplotlib.pyplot as plt
 
+t = np.arange(0.0,3.0,0.01)
+fig = plt.figure(1)
 
 def IPPlot(root):
-    return 0
-
-fig = plt.figure(1)
-plt.ion()
-t = np.arange(0.0,3.0,0.01)
-s = np.sin(np.pi*t)
-plt.plot(t,s)
-
-canvas = FigureCanvasTkAgg(fig, master=root)
-plot_widget = canvas.get_tk_widget()
+    plt.ion()
+    s = np.sin(np.pi*t)
+    plt.plot(t,s)
+    canvas = FigureCanvasTkAgg(fig, master=root)
+    plot_widget = canvas.get_tk_widget()
+    plot_widget.grid(row=0, column=0)
+    updateButton = tk.Button(root, text="Update", command=update).grid(row=1, column=0)
+    return plot_widget, updateButton
 
 def update():
     s = np.cos(np.pi*t)
     plt.plot(t,s)
-    #d[0].set_ydata(s)
     fig.canvas.draw()
 
-plot_widget.grid(row=0, column=0)
-tk.Button(root,text="Update",command=update).grid(row=1, column=0)
-root.mainloop()
+
--- IntegratedProject/GUI/IPMGUI/MainGUI.py
+++ IntegratedProject/GUI/IPMGUI/MainGUI.py
@@ -5,6 +5,9 @@ import IPPlot as iplt
 root = tk.Tk()
 
 menubar = mb.MenuBar(root)
+thePlot, button = iplt.IPPlot(root)
 
 root.config(menu=menubar)
+# root.config(canvas=thePlot)
+root.config(button=button)
 root.mainloop()
